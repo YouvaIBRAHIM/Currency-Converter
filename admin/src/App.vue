78d3cddfed96d7b226313285453803940aee8f8f
YouvaIBRAHIM <youva.ibrahim@gmail.com>
2023-07-19 19:53:54 +0200

    project init

---
+++ admin/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NavBar from "@/components/NavBar.vue";
+import { store } from "@/services/auth.js";
+
+</script>
+
+<template>
+  <header>
+    <NavBar  v-if="store.user"/>
+  </header>
+  <main>
+    <router-view />
+  </main>
+</template>
